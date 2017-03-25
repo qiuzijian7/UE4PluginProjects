7eb56925d787f4b3045d4f2aeb9c946de69ec54d
qiuzijian7 <919868412@qq.com>
2017-03-25 21:39:01 +0800

    init ProjectFiles

---
+++ Source/PluginProjects/MyClass.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+/**
+ * 
+ */
+class PLUGINPROJECTS_API MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+};
